4a5a1d33c250262c43e92f5909581358114a495e
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2019-10-10 00:33:29 +0200

    updated index headline

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,8 +11,7 @@ const IndexPage = () => {
     <>
     <Layout>
         <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
+        <h1>Kenny vs Spenny Gatsby Demo</h1>
         <p>Now go build something great.</p>
         <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
           <Image />
